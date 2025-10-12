0452a2769415f8063285113ea4ea00a4725b2eb6
sjthesahiljoseph <sjthesahiljoseph@gmail.com>
2025-10-12 23:03:23 +0500

    initialized

---
+++ code/build.bat
@@ -0,0 +1,11 @@
+
+
+@echo off
+
+pushd ..\build
+
+cl -nologo -Zi ..\code\win32_platform.c
+
+popd
+
+
---
+++ code/win32_platform.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+
+int main(int argc, char const* argv[]) {
+
+	printf("TenPmNight\n");
+
+	return 0;
+}
+
+
