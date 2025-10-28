4b6d21884239cb5e7636b6757a6d5ac5ac36be46
sjthesahiljoseph <sjthesahiljoseph@gmail.com>
2025-10-28 10:55:24 +0500

    build.bat ++

--- code/build.bat
+++ code/build.bat
@@ -4,9 +4,17 @@
 
 if not exist ..\build mkdir ..\build
 
+
+
 pushd ..\build
 
+del *.pdb > NUL 2 > NUL
+echo WAITING FOR PDB > lock.tmp
+
 cl -nologo -Zi -FC ..\code\win32_platform.c user32.lib gdi32.lib
+
+del lock.tmp
+del *.obj
 
 win32_platform.exe
 
